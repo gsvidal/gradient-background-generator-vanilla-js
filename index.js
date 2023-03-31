8a5e4f64557d22f5b821c2bd36f74359ffe84f71
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-03-30 23:23:03 -0500

    Add range input for middle color

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const colorMiddleElement = document.querySelector('#middle');
 const colorRightElement = document.querySelector('#right');
 const bodyElement = document.querySelector('body');
 const titleElement = document.querySelector('h1');
+const rangeInputElement = document.querySelector('#range');
 
 // Change title color if only middle color is darker (R,G and B channels are less than "80" respectively)
-const changeColor = (event, position) => {
+const handleChangeColor = (event, position) => {
   if(position === "middle") {
     let s = event.target.value;
     if (s.slice(1, 3) < '80' && s.slice(3, 5) < '80' && s.slice(5, 7) < '80') {
@@ -18,8 +19,15 @@ const changeColor = (event, position) => {
   bodyElement.style.background = `linear-gradient(to right, ${colorLeftElement.value}, ${colorMiddleElement.value}, ${colorRightElement.value})`;
 };
 
-colorLeftElement.addEventListener('input', changeColor);
+const handleChangeRange = (event) => {
+  const value = event.target.value;
+  bodyElement.style.background = `linear-gradient(to right, ${colorLeftElement.value}, ${colorMiddleElement.value} ${value}%, ${colorRightElement.value})`;
+}
 
-colorMiddleElement.addEventListener('input', (event) => changeColor(event, "middle"));
+colorLeftElement.addEventListener('input', handleChangeColor);
 
-colorRightElement.addEventListener('input', changeColor);
+colorMiddleElement.addEventListener('input', (event) => handleChangeColor(event, "middle"));
+
+colorRightElement.addEventListener('input', handleChangeColor);
+
+rangeInputElement.addEventListener('input', handleChangeRange);
