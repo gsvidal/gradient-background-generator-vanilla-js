2bacddf4e9e508cc4e6c098d59708a3bb5825c78
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-03-31 00:01:30 -0500

    Fix color for all elements in body and add angle orientation feature

--- index.js
+++ index.js
@@ -2,32 +2,43 @@ const colorLeftElement = document.querySelector('#left');
 const colorMiddleElement = document.querySelector('#middle');
 const colorRightElement = document.querySelector('#right');
 const bodyElement = document.querySelector('body');
-const titleElement = document.querySelector('h1');
+// const titleElement = document.querySelector('h1');
 const rangeInputElement = document.querySelector('#range');
+const angleInputElement = document.querySelector('#angle');
 
 // Change title color if only middle color is darker (R,G and B channels are less than "80" respectively)
 const handleChangeColor = (event, position) => {
-  if(position === "middle") {
+  if (position === 'middle') {
     let s = event.target.value;
     if (s.slice(1, 3) < '80' && s.slice(3, 5) < '80' && s.slice(5, 7) < '80') {
-      titleElement.style.color = 'white';
+      bodyElement.style.color = 'white';
     } else {
-      titleElement.style.color = 'black';
+      bodyElement.style.color = 'black';
     }
   }
 
-  bodyElement.style.background = `linear-gradient(to right, ${colorLeftElement.value}, ${colorMiddleElement.value}, ${colorRightElement.value})`;
+  bodyElement.style.background = `linear-gradient(${angleInputElement.value}deg, ${colorLeftElement.value}, ${colorMiddleElement.value} ${rangeInputElement.value}%, ${colorRightElement.value})`;
 };
 
 const handleChangeRange = (event) => {
   const value = event.target.value;
-  bodyElement.style.background = `linear-gradient(to right, ${colorLeftElement.value}, ${colorMiddleElement.value} ${value}%, ${colorRightElement.value})`;
-}
+  bodyElement.style.background = `linear-gradient(${angleInputElement.value}deg, ${colorLeftElement.value}, ${colorMiddleElement.value} ${value}%, ${colorRightElement.value})`;
+};
+
+const handleChangeAngle = (event) => {
+  console.log(event.target.value);
+  const value = event.target.value;
+  bodyElement.style.background = `linear-gradient(${value}deg, ${colorLeftElement.value}, ${colorMiddleElement.value} ${rangeInputElement.value}%, ${colorRightElement.value})`;
+};
 
 colorLeftElement.addEventListener('input', handleChangeColor);
 
-colorMiddleElement.addEventListener('input', (event) => handleChangeColor(event, "middle"));
+colorMiddleElement.addEventListener('input', (event) =>
+  handleChangeColor(event, 'middle')
+);
 
 colorRightElement.addEventListener('input', handleChangeColor);
 
 rangeInputElement.addEventListener('input', handleChangeRange);
+
+angleInputElement.addEventListener('input', handleChangeAngle);
