f8dea8ea82e0801560570309b8323ebe71cf835c
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-03-31 00:08:20 -0500

    Fix range and number input labels

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const colorLeftElement = document.querySelector('#left');
 const colorMiddleElement = document.querySelector('#middle');
 const colorRightElement = document.querySelector('#right');
 const bodyElement = document.querySelector('body');
-// const titleElement = document.querySelector('h1');
 const rangeInputElement = document.querySelector('#range');
+const labelRangeElement = document.querySelector('#label-range');
 const angleInputElement = document.querySelector('#angle');
+const labelAngleElement = document.querySelector('#label-angle');
+
 
 // Change title color if only middle color is darker (R,G and B channels are less than "80" respectively)
 const handleChangeColor = (event, position) => {
@@ -23,12 +25,13 @@ const handleChangeColor = (event, position) => {
 const handleChangeRange = (event) => {
   const value = event.target.value;
   bodyElement.style.background = `linear-gradient(${angleInputElement.value}deg, ${colorLeftElement.value}, ${colorMiddleElement.value} ${value}%, ${colorRightElement.value})`;
+  labelRangeElement.textContent = `${value}%`;
 };
 
 const handleChangeAngle = (event) => {
-  console.log(event.target.value);
   const value = event.target.value;
   bodyElement.style.background = `linear-gradient(${value}deg, ${colorLeftElement.value}, ${colorMiddleElement.value} ${rangeInputElement.value}%, ${colorRightElement.value})`;
+  labelAngleElement.textContent = `${value}°`;
 };
 
 colorLeftElement.addEventListener('input', handleChangeColor);
